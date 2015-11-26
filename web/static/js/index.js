36da340d2f19bbc9f22e4e8d5b2cd411d69fbef9
Ben Smith <ben@10consulting.com>
2015-11-26 21:27:57 +0000

    Add Redux DevTools
    https://github.com/gaearon/redux-devtools

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -2,23 +2,33 @@ import React from 'react';
 import { render } from 'react-dom';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import App from './containers/App';
 import todoApp from './reducers';
 
 const loggerMiddleware = createLogger();
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware, // lets us dispatch() functions
-  loggerMiddleware // neat middleware that logs actions
+const createStoreWithMiddleware = compose(
+  applyMiddleware(
+    thunkMiddleware, // lets us dispatch() functions
+    loggerMiddleware // neat middleware that logs actions
+  ),
+  devTools()
 )(createStore);
 
 const store = createStoreWithMiddleware(todoApp);
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
   document.getElementById('root')
 );
